18edf7d193fb825c565c5887cfba2fd83a36fa86
DaraSouza <147327514+DaraSouza@users.noreply.github.com>
2023-11-19 15:11:18 +0000

    added new files

---
+++ list_assignment.py
@@ -0,0 +1,23 @@
+fruits = ["apple", "banana","orange", "grape", "watermelon"]
+print("List of fruits:", fruits)
+
+numbers = [1, 2, 3, 4, 5]
+print("List of numbers:", numbers)
+
+mixed_list = ["apple", 2, True, 4.5, "banana"]
+print("Mixed list:", mixed_list)
+
+first_fruit = fruits[0]
+print("First fruit:", first_fruit)
+
+second_number = numbers[1]
+print("Second number", second_number)
+
+fruits.append("pear")
+print("Updated list of fruits:", fruits)
+
+numbers.insert(2, 6)
+print("Updated list of numbers:", numbers)
+
+mixed_list.remove(True)
+print("Updated mixed list:", mixed_list)
